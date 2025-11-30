8216efc27e3b8340537f803834160ca7f7fa667d
flanter21 <flanter21@gmail.com>
2025-11-30 22:26:36 +0000

    Add device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_iPlay_70_mini_Ultra.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_iPlay_70_mini_Ultra-user \
+    lineage_iPlay_70_mini_Ultra-userdebug \
+    lineage_iPlay_70_mini_Ultra-eng
---
+++ lineage_iPlay_70_mini_Ultra.mk
@@ -0,0 +1,26 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from iPlay_70_mini_Ultra device
+$(call inherit-product, device/alldocube/iPlay_70_mini_Ultra/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := iPlay_70_mini_Ultra
+PRODUCT_NAME := lineage_iPlay_70_mini_Ultra
+PRODUCT_BRAND := ALLDOCUBE
+PRODUCT_MODEL := iPlay 70 mini Ultra
+PRODUCT_MANUFACTURER := alldocube
+
+PRODUCT_GMS_CLIENTID_BASE := android-ragentek
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="iPlay_70_mini_Ultra-user 14 UKQ1.240624.001 109 release-keys" \
+    BuildFingerprint := ALLDOCUBE/iPlay_70_mini_Ultra/iPlay_70_mini_Ultra:14/UKQ1.240624.001/109:user/release-keys
