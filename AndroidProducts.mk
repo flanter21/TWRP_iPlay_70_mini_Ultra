bb960bbf8f164e1f97590cafec15485fae9c07da
flanter21 <flanter21@gmail.com>
2025-12-01 20:25:37 +0000

    Go back to aospdtgen tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,12 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
+# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_iPlay_70_mini_Ultra.mk
+    $(LOCAL_DIR)/lineage_iPlay_70_mini_Ultra.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_iPlay_70_mini_Ultra-user \
-    twrp_iPlay_70_mini_Ultra-userdebug \
-    twrp_iPlay_70_mini_Ultra-eng
+    lineage_iPlay_70_mini_Ultra-user \
+    lineage_iPlay_70_mini_Ultra-userdebug \
+    lineage_iPlay_70_mini_Ultra-eng
--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ```
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
+# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 ```
