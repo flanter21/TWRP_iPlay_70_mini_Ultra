e50081baf995c12a718c4fab09548eb7ab3e75ed
flanter21 <139064898+flanter21@users.noreply.github.com>
2025-12-02 02:24:55 +0300

    Replace lineage with twrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,9 +4,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_iPlay_70_mini_Ultra.mk
+    $(LOCAL_DIR)/twrp_iPlay_70_mini_Ultra.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_iPlay_70_mini_Ultra-user \
-    lineage_iPlay_70_mini_Ultra-userdebug \
-    lineage_iPlay_70_mini_Ultra-eng
+    twrp_iPlay_70_mini_Ultra-user \
+    twrp_iPlay_70_mini_Ultra-userdebug \
+    twrp_iPlay_70_mini_Ultra-eng
